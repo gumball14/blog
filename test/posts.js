6a95f144bc6ec1b05f46f3978ce713c769036529
Eren Jeager <55504916+gumball14@users.noreply.github.com>
2023-05-09 13:08:22 +0800

    Add files via upload

---
+++ test/posts.js
@@ -0,0 +1,34 @@
+function getPostContents(postFile) {
+    $.getJSON("https://raw.githubusercontent.com/gumball14/blog/main/test/" + postFile, function (data) {
+        $("#postTitleEntry").text(data.title);
+        $("#postBodyContent").text(data.body);
+        $("#postBodyContentSnippet").text(data.body.substr(0, 700) + "...");
+    });
+}
+
+const url = 'https://api.github.com/repos/gumball14/blog/contents/test';
+
+if (typeof data !== 'undefined' && data.view.isSingleItem) {
+    // This is a single post
+    console.log('This is a single post');
+} else {
+    // This is not a single post
+    console.log('This is not a single post');
+}
+
+const idFile = $(document).find("title").text() + ".json";
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(`length: ${data.length}; id: ${idFile}`);
+        for (let i = 0; i < data.length; i++) {
+            const file = data[i];
+            console.log(file.name);
+            if (file.type === 'file' && file.name.toLowerCase() === idFile.toLowerCase()) {
+                getPostContents(idFile);
+                break;
+            } else {
+                getPostContents(file.name);
+            }
+        }
+    });
