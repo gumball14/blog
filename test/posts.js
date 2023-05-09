83a7e6cf7be8142f8f943e261e5924a445c35c92
Eren Jeager <55504916+gumball14@users.noreply.github.com>
2023-05-09 16:19:18 +0800

    Update posts.js

--- test/posts.js
+++ test/posts.js
@@ -1,20 +1,24 @@
 function getPostContents(postFile) {
-    $.getJSON("https://raw.githubusercontent.com/gumball14/blog/main/test/posts" + postFile, function (data) {
+    $.getJSON("https://raw.githubusercontent.com/gumball14/blog/main/test/posts/" + postFile, function (data) {
         $("#postTitleEntry").text(data.title);
         $("#postBodyContent").text(data.body);
         $("#postBodyContentSnippet").text(data.body.substr(0, 700) + "...");
     });
 }
 
-const url = 'https://api.github.com/repos/gumball14/blog/contents/test/posts';
 
-if (typeof data !== 'undefined' && data.view.isSingleItem) {
-    console.log('single item');
-    const idFile = $(document).find("title").text() + ".json";
-    getPostContents(idFile);
-} else {
-    console.log('multiple items');
-    const titleEntry = document.getElementById('postTitleEntry');
-    const fileId = titleEntry.innerText;
-    getPostContents(fileId);
-}
+fetch('https://api.github.com/repos/gumball14/blog/contents/test/posts')
+        .then(response => response.json())
+        .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            const file = data[i];
+            const titleEntry = document.getElementById('postTitleEntry');
+            const fileId = titleEntry.innerText + '.json';
+            console.log(fileId + ' ' + file.name);
+            if (file.type == 'file' && file.name.toLowerCase() === fileId.toLowerCase()) {
+                console.log('inside if');
+                getPostContents(data[i].name);
+				break;
+            }
+        }
+    });
