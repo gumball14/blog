9320a71654ccedf958262986f1bc7b5cc1b12700
Eren Jeager <55504916+gumball14@users.noreply.github.com>
2023-05-09 14:50:37 +0800

    Update posts.js

--- test/posts.js
+++ test/posts.js
@@ -1,12 +1,12 @@
 function getPostContents(postFile) {
-    $.getJSON("https://raw.githubusercontent.com/gumball14/blog/main/test/" + postFile, function (data) {
+    $.getJSON("https://raw.githubusercontent.com/gumball14/blog/main/test/posts" + postFile, function (data) {
         $("#postTitleEntry").text(data.title);
         $("#postBodyContent").text(data.body);
         $("#postBodyContentSnippet").text(data.body.substr(0, 700) + "...");
     });
 }
 
-const url = 'https://api.github.com/repos/gumball14/blog/contents/test';
+const url = 'https://api.github.com/repos/gumball14/blog/contents/test/posts';
 
 if (typeof data !== 'undefined' && data.view.isSingleItem) {
     console.log('single item');
